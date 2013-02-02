4d447d24d26828b8a9ee409d144546c82b2e8350
egoing <egoing@gmail.com>
2013-02-02 13:33:53 +0900

    * 실습 내용과 저장소의 내용이 달라서 동기화

--- index.php
+++ index.php
@@ -10,58 +10,108 @@ mysql_query("set session character_set_connection=utf8;");
 mysql_query("set session character_set_results=utf8;");
 mysql_query("set session character_set_client=utf8;");
 if(!empty($_GET['id'])) {
-	$sql="SELECT * FROM topic WHERE id = ".mysql_real_escape_string($_GET['id']);
-	$result = mysql_query($sql);
-	$topic = mysql_fetch_assoc($result);
+$sql="SELECT * FROM topic WHERE id = ".$_GET['id'];
+$result = mysql_query($sql);
+$topic = mysql_fetch_assoc($result);
 }
 ?>
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8" />
-		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
-		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
-		Remove this if you use the .htaccess -->
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-
-		<title>index</title>
-		<meta name="description" content="" />
-		<meta name="author" content="egoing" />
-
-		<meta name="viewport" content="width=device-width; initial-scale=1.0" />
-
-		<!-- Replace favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
-		<link rel="shortcut icon" href="/favicon.ico" />
-		<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-		<link rel="stylesheet" type="text/css" href="./style.css" />
-	</head>
-
-	<body id="body" style="font-size:1em">
-		<div>
-			<header>
-				<h1>Opentutorials by egoing</h1>
-			</header>
-			<div id="toolbar">
-				<input type="button" value="black" onclick="document.getElementById('body').className='black'" />
-				<input type="button" value="white" onclick="document.getElementById('body').className='white'" />
-			</div>
-			<nav>
-				<ul>
-					<?php
-					$sql="select id,title from topic";
-					$result=mysql_query($sql);
-					while($row=mysql_fetch_assoc($result)) {
-					echo "<li><a href=\"?id={$row['id']}\">{$row['title']}</a></li>";
-					}
-					?>
-				</ul>
-			</nav>
-			<article>
-				<h2><?=$topic['title']?></h2>
-				<div>
-					<?=$topic['description']?>
-				</div>
-			</article>
-		</div>
-	</body>
+    <head>
+        <meta charset="utf-8" />
+        <style type="text/css">
+            body {
+                font-size: 0.8em;
+                font-family: dotum;
+                line-height: 1.6em;
+            }
+            body.black {
+                background-color: black;
+                color: white;
+            }
+            body.black a {
+                color: white;
+            }
+            body.black a:hover {
+                color: #f60;
+            }
+            body.black header {
+                border-bottom: 1px solid #333;
+            }
+            body.black nav {
+                border-right: 1px solid #333;
+            }
+            header {
+                border-bottom: 1px solid #ccc;
+                padding: 20px 0;
+            }
+            #toolbar {
+                padding: 10px;
+                float: right;
+            }
+            nav {
+                float: left;
+                margin-right: 20px;
+                min-height: 500px;
+                border-right: 1px solid #ccc;
+            }
+            nav ul {
+                list-style: none;
+                padding-left: 0;
+                padding-right: 20px;
+            }
+            article {
+                float: left;
+            }
+            footer {
+                clear: both;
+            }
+            a {
+                text-decoration: none;
+            }
+            a:link, a:visited {
+                color: #333;
+            }
+            a:hover {
+                color: #f60;
+            }
+            h1 {
+                font-size: 1.4em;
+            }
+            .description{
+                width:500px;
+            }
+        </style>
+    </head>
+ 
+    <body id="body">
+        <div>
+            <header>
+                <h1>JavaScript</h1>
+            </header>
+            <div id="toolbar">
+                <input type="button" value="black" onclick="document.getElementById('body').className='black'" />
+                <input type="button" value="white" onclick="document.getElementById('body').className='white'" />
+            </div>
+            <nav>
+                <ul>
+                    <?php
+                    $sql="select id,title from topic";
+                    $result=mysql_query($sql);
+                    while($row=mysql_fetch_assoc($result)) {
+                    echo "
+                    <li>
+                        <a href=\"?id={$row['id']}\">{$row['title']}</a></li>";
+                        }
+                        ?>
+                </ul>
+            </nav>
+            <article>
+                <h2><?=$topic['title']?></h2>
+                <div class="description">
+                    <?=$topic['description']?>
+                </div>
+            </article>
+        </div>
+    </body>
 </html>
